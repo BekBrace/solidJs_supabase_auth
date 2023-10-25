235272cfaa74bf6861da16611772d6b153414b67
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2023-10-25 09:14:40 +0200

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,11 @@
-
+// importing createsignal and createsignal functions
 import { createEffect, createSignal } from "solid-js";
+// usenavigate for routing
 import { useNavigate } from "@solidjs/router";
-
+// createclient is used to create a supabase client
 import { createClient } from "@supabase/supabase-js";
 
 const Dashboard = () => {
-    
     const [user, setUser] = createSignal({}); // user details
     const navigate = useNavigate();
     const supabase = createClient("https://jsqoravefyofyjxbmrij.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcW9yYXZlZnlvZnlqeGJtcmlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1NjAzMzcsImV4cCI6MjAxMzEzNjMzN30.XtlCFoTqFB0AeFTHtLVjQL-3-riy_mCEEepAfnGUBQI");
