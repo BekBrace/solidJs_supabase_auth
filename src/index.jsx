e9a6a2fec06506ce0b1d3fe5d94eaf08bc733a58
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2023-10-25 09:08:27 +0200

    Update index.jsx
    adding comments on each code line

--- src/index.jsx
+++ src/index.jsx
@@ -5,17 +5,16 @@ import { Router } from "@solidjs/router";
 import { createClient } from '@supabase/supabase-js';
 import { SupabaseProvider } from 'solid-supabase';
 
-import './index.css';
-import App from './App';
-
+import './index.css'; // styling
+import App from './App'; // main application rendered below
+// this is a supabase client
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
-
+// render function for the app wrapped in router tags, wrapped in supabaseprovider connecting 
+// supabase client
 render(() =>
-
      <SupabaseProvider client={supabase}>
         <Router>
             <App />
         </Router>
      </SupabaseProvider> 
-
 , document.getElementById('root'));
