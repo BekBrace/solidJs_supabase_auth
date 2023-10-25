eb62fd8e89e633a3e821f303019932cd9eb4aa85
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2023-10-25 09:04:59 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
+// Importing render for template rendering, router for routing and redirecting pages, createClient 
+// for creating a supabase client, and lastly supabaseprovider for client connection
 import { render } from 'solid-js/web';
 import { Router } from "@solidjs/router";
 import { createClient } from '@supabase/supabase-js';
 import { SupabaseProvider } from 'solid-supabase';
-
-
-
 
 import './index.css';
 import App from './App';
